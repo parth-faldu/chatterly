40026051d06425235d73c862e0d89c7e77fbe978
parth-faldu <parth123faldu@gmail.com>
2025-05-03 19:19:38 +0530

    fix: update CORS origin format and adjust BASE_URL for development environment

--- frontend/src/store/UseAuthStore.jsx
+++ frontend/src/store/UseAuthStore.jsx
@@ -1,12 +1,10 @@
 import { create } from "zustand";
-import { axiosInstance } from "../lib/axios";
+import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5000/api"
-    : "/";
+  import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
