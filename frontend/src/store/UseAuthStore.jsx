08a9e9872e86abd3e858f11a25e36c72ed98c5da
parth-faldu <parth123faldu@gmail.com>
2025-05-03 14:05:15 +0530

    fix: update BASE_URL to use root path for production environment

--- frontend/src/store/UseAuthStore.jsx
+++ frontend/src/store/UseAuthStore.jsx
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5000/api"
-    : window.location.origin;
+    : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
