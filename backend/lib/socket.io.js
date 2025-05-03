eaad7fd9049509d0303d4e82bd6a45da99823386
parth-faldu <parth123faldu@gmail.com>
2025-05-03 20:46:30 +0530

    fix: load dotenv configuration in socket.io and update axios baseURL to use CORS_ORIGIN

--- backend/lib/socket.io.js
+++ backend/lib/socket.io.js
@@ -1,6 +1,9 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
